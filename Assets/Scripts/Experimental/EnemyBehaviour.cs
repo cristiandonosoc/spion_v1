4a11c3e0cfdcc91731695eca21dc5e047b8843f8
Cristián Donoso <cristiandonosoc@gmail.com>
2017-06-03 13:57:28 -0700

    Explostion

--- Assets/Scripts/Experimental/EnemyBehaviour.cs
+++ Assets/Scripts/Experimental/EnemyBehaviour.cs
@@ -9,15 +9,30 @@ public class EnemyBehaviour : CustomMonoBehaviour {
         HIT
     }
 
+    [SerializeField]
     private SparksParticleSystemBehaviour _sparksParticleSystem;
     public SparksParticleSystemBehaviour SparksParticleSystem {
         get {
+            if (_sparksParticleSystem == null) {
+                _sparksParticleSystem = GetComponentInChildren<SparksParticleSystemBehaviour>();
+            }
             return _sparksParticleSystem;
         }
     }
 
+    [SerializeField]
+    private ParticleSystem _explosionParticleSystemPrefab;
+    public ParticleSystem ExplosionParticleSystemPrefab {
+        get {
+            return _explosionParticleSystemPrefab;
+        }
+    }
+
+    private HealthComponentBehaviour _healthComponent;
+
     protected override void PlayModeAwake() {
         _sparksParticleSystem = GetComponentInChildren<SparksParticleSystemBehaviour>();
+        _healthComponent = GetComponent<HealthComponentBehaviour>();
     }
 
     public override void ReceiveMessage(Message message) {
@@ -27,6 +42,21 @@ public class EnemyBehaviour : CustomMonoBehaviour {
             if (msg == MessageKind.HIT) {
                 Log("HIT");
                 SparksParticleSystem.Play();
+
+                if (_healthComponent != null) {
+                    _healthComponent.CurrentHP -= 3;
+
+                    if (_healthComponent.CurrentHP == 0) {
+                        if (ExplosionParticleSystemPrefab != null) {
+                            var explosion = Instantiate<ParticleSystem>(ExplosionParticleSystemPrefab);
+                            explosion.transform.position = transform.position;
+                            explosion.Play();
+                            Destroy(gameObject);
+                            // We destroy the explostion after a while
+                            Destroy(explosion.gameObject, explosion.main.duration);
+                        }
+                    }
+                }
             }
         }
     }
